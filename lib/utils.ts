bfdbcdcd51f7c5caaf8d099fa02a64061bd43683
Endri Simoni <endri.simoni1@gmail.com>
2024-05-16 23:29:57 +0200

    Create api for accounts, categories, transactions, create page for accounts & transactions

---
+++ lib/utils.ts
@@ -0,0 +1,14 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
+
+export function covertAmountFromMiliunits(amount: number) {
+    return amount / 1000;
+}
+
+export function convertAmountToMiliunits(amount: number) {
+    return Math.round(amount * 1000);
+}
